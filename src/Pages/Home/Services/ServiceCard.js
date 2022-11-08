65d7f19f2a546510d3a570f2e9320e2a4ac3c476
imranfix <imran2000hosan@gmail.com>
2022-11-08 19:22:44 +0600

    added carousel slider and 6 banner photo with navbar

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ServiceCard = ({service}) => {
+    const {_id, image, fees, title, details} = service;
+
+    return (
+        <div className="card card-compact w-62 bg-base-100 shadow-xl">
+    <figure><img className='p-3' src={image} alt="Shoes" /></figure>
+         <div className="card-body">
+    <h2 className="card-title">{title}</h2>
+    <p className='text-2xl text-orange-600 font-semibold'>Fees: {fees}</p>
+    <p>Details: {details}</p>
+    <div className="card-actions justify-end">
+      <Link to={`/checkout/${_id}`}>
+      <button className="btn btn-primary">Checkout</button>
+      </Link>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default ServiceCard;
