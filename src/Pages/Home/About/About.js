776480f52d622bacf3cf9c15d1631ccb817acf9c
imranfix <imran2000hosan@gmail.com>
2022-11-08 16:04:49 +0600

    added navbar section

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import about1 from '../../../Assets/about-img/about1.webp'
+import about2 from '../../../Assets/about-img/about2.jpg'
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen my-20 bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+        <div className='relative w-1/2'>
+        <img src={about1} alt='' className="w-4/5 h-full rounded-lg shadow-2xl" />
+        <img src={about2} alt='' className="absolute w-3/5 right-5 top-1/2 border-8 rounded-lg shadow-2xl" />
+        </div>
+
+          <div className='w-1/2'>
+          <p className=' text-2xl text-orange-600'>About Us</p>
+            <h1 className="text-5xl my-5 font-bold">We are qualified & of experience in this field</h1>
+            <p className="py-6"> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <p className='py-6'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            <button className="btn btn-primary">Get More Info</button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default About;
---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+const BannerItem = ({slide}) => {
+    const {image, id, prev, next} = slide;
+
+
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+        <div className='carousel-img'>
+            <img src={image} alt="" className='w-full rounded-xl' />
+        </div>
+
+          <div className=" absolute flex justify-end transform -translate-y-1/2 left-24 top-1/2 w-2/5">
+           <p className='text-white text-xl mt-14'>
+               There Are Many Variactions of Passages of Available, But The Majority Have Suffered Alteration in Some Form
+           </p>
+          </div>
+
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4 ">
+           <h1 className='text-6xl text-white font-bold'>
+                Informative <br />
+                helping for Immigration <br />
+               and Consultancy
+           </h1>
+          </div>
+
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a> 
+            <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+    );
+};
+
+export default BannerItem;
